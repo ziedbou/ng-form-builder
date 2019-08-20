8c695fc81b54964233b7497936e0de903ecc3280
Zied Bouhejba <bouhejbazied@gmail.com>
2019-08-20 21:59:32 +0100

    initial commit

---
+++ projects/form-builder/src/lib/form-builder.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FormBuilderComponent } from './form-builder.component';
+
+
+
+@NgModule({
+  declarations: [FormBuilderComponent],
+  imports: [
+  ],
+  exports: [FormBuilderComponent]
+})
+export class FormBuilderModule { }
